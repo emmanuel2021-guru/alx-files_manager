744dc4eb1d84ccc695dc028d5566f55511100db6
emmanuel <emmanuellawakin2005@gmail.com>
2024-05-31 16:39:09 +0100

    Finished tasks 1 and 2

---
+++ controllers/AppController.js
@@ -0,0 +1,26 @@
+// controllers/AppController.js
+
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    const stats = {
+      users: users,
+      files: files,
+    };
+    res.status(200).json(stats);
+  }
+}
+
+module.exports = AppController;
---
+++ utils/db.js
@@ -0,0 +1,37 @@
+// utils/db.js
+
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+        console.log('MongoDB client connected successfully');
+      })
+      .catch((error) => {
+        console.error(`MongoDB client not connected to the server: ${error.message}`);
+      });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
