d8dcc0ae3d8e951789c6982c7b70feaeac52a083
emmanuel <emmanuellawakin2005@gmail.com>
2024-05-31 18:40:13 +0100

    Completed task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto');
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class UsersController {
   static async postNew(req, res) {
@@ -33,6 +34,32 @@ class UsersController {
 
     const user = result.ops[0];
     return res.status(201).json({ id: user._id, email: user.email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const tokenKey = `auth_${token}`;
+    const userId = await redisClient.get(tokenKey);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({
+      _id: dbClient.ObjectId(userId),
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
